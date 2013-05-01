eb680f01f8bf469c3eaa858fa95716dd090c39db
James Snee <james.snee@gmail.com>
2013-05-01 17:05:40 +0100

    Initial Commit

---
+++ James Snee WWDC/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  James Snee WWDC
+//
+//  Created by James Snee on 01/05/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ James Snee WWDC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  James Snee WWDC
+//
+//  Created by James Snee on 01/05/2013.
+//  Copyright (c) 2013 James Snee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
